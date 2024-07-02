88ee57af2f4d6b53b41fd5ec1407162d3940ee81
azzaomer <azzaomer70@gmail.com>
2024-07-01 18:41:00 -0700

    creat file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creating a file in/tmp using Puppet
+
+file {'Create a file':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
