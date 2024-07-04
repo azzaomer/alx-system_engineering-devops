8ce0492741a9870e4cc02aa35b9c889d1a073f4b
azzaomer <azzaomer70@gmail.com>
2024-07-03 18:06:22 -0700

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -13,10 +13,11 @@ file {'/var/www/html/index.html':
   content => 'Hello World!'
 }
 
-exec {'redirect_me':
-  command => 'sed -i "24i\	rewrite ^/redirect_me https://www.google.com/
-					permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
+exec {'redirection-301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service {'nginx':
