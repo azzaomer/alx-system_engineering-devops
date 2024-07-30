68264fe2be4d071dc7685dbc5947862f38138cda
azzaomer <azzaomer70@gmail.com>
2024-07-30 15:57:01 -0700

    tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,37 +1,45 @@
+#!/usr/bin/python3
+"Python script that exports data to a JSON format"
+import json
 import requests
 import sys
 
 
-def tasks(id):
+def tasks_completed():
     '''Script that exports an employee TODO tasks to a json file
         Parameters:
         employee_id: Is an interger representing an employee id.
     '''
 
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    response = requests.get(url)
-    response_json = response.json()
-    employee_name = response_json.get("name")
+    id = 1
+    tasks_todo = {}
+    while True:
+        url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+        response = requests.get(url)
+        response_json = response.json()
+        if len(response_json) == 0:
+            break
+        employee_name = response_json.get("name")
 
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    todos = requests.get(url)
-    todos_json = todos.json()
-    task_list = []
+        url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        todos = requests.get(url)
+        todos_json = todos.json()
+        task_list = []
 
-    for task in todos_json:
-        task_dict = {}
-        task_dict["task"] = task.get("title")
-        task_dict["completed"] = task.get("completed")
-        task_dict["username"] = employee_name
-        task_list.append(task_dict)
+        for task in todos_json:
+            task_dict = {}
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            task_dict["username"] = employee_name
+            task_list.append(task_dict)
 
-    todos[id] = task_list
-    id += 1
+        tasks_todo[id] = task_list
+        id += 1
 
     file_name = "todo_all_employees.json"
     with open(file_name, "a") as f:
-        json.dump(todos, f)
+        json.dump(tasks_todo, f)
 
 
 if __name__ == "__main__":
-    tasks(sys.argv[1])
+    tasks_completed()
