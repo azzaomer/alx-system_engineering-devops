cc35b604d3f0cb794a6fe8faa83cd798827cb037
azzaomer <azzaomer70@gmail.com>
2024-08-13 12:59:54 +0300

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Task 0
+"""
+def top_ten(subreddit):
+    """
+     a function that queries
+     the Reddit API and prints the titles
+     of the first 10 hot posts listed for
+     a given subreddit.
+    """
+    import requests
+    header = {"User-Agent": "My-User-Agent"}
+    sub_reddit = requests.get("http://www.reddit.com/r/{}/hot.json?limit=10"
+                              .format(subreddit),
+                              headers=header,
+                              allow_redirects=False)
+    if sub_reddit.status_code >= 300:
+        return None
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_reddit.json().get("data").get("children")]
