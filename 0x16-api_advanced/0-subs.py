a7a9a0faedc7d7689ed6f1770b2e36ef01b524a1
azzaomer <azzaomer70@gmail.com>
2024-08-14 14:03:27 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,6 +3,7 @@
 Task 0
 """
 import requests # type: ignore
+import json
 
 
 def number_of_subscribers(subreddit):
@@ -17,11 +18,19 @@ def number_of_subscribers(subreddit):
     url = ("https://api.reddit.com/r/{}/about".format(subreddit))
     header = {"User-Agent": "My-User-Agent"}
     sub_reddit = requests.get(url, headers=header, allow_redirects=False)
-    
+
     if sub_reddit.status_code >= 300:
         return 0
     sub_reddit = sub_reddit.json()
+    if sub_reddit.status_code == 200:  # Check for a successful response
+        try:
+            data = sub_reddit.json()  # Attempt to parse JSON
+        except json.JSONDecodeError:
+            print("Failed to parse JSON: Empty or invalid JSON content.")
+    else:
+        print(f"Request failed with status code {sub_reddit.status_code}")
     if 'data' in sub_reddit:
         return (sub_reddit.get('data').get('subscribers'))
+    
     else:
         return 0
