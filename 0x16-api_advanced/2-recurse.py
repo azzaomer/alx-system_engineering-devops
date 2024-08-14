72d2ed33c507ef60e7f22637d042b963ec3d446e
azzaomer <azzaomer70@gmail.com>
2024-08-14 14:41:37 +0300

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,6 +4,9 @@ Task 2
 """
 
 
+import requests
+
+
 def recurse(subreddit, hot_list=[]):
     """
      a function that queries
@@ -11,19 +14,29 @@ def recurse(subreddit, hot_list=[]):
      of the first 10 hot posts listed for
      a given subreddit.
     """
-    import requests
-    header = {"User-Agent": "My-User-Agent"}
-    sub_reddit = requests.get("http://www.reddit.com/r/{}/hot.json"
-                              .format(subreddit),
-                              headers=header,
-                              allow_redirects=False)
+    if type(subreddit) is list:
+        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit[0])
+        url = "{}&after={}".format(url, subreddit[1])
+    else:
+        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit)
+        subreddit = [subreddit, ""]
+    header = {'User-Agent': 'CustomClient/1.0'}
+    sub_reddit = requests.get(url, headers=header, allow_redirects=False)
     if sub_reddit.status_code >= 400:
         return None
-    hot = hot_list + [child.get("data").get("title")
-                      for child in sub_reddit.json()
-                      .get("data").get("children")]
-    sub = sub_reddit.json()
-    if not sub.get("data").get("after"):
-        return hot
-    return recurse(subreddit, hot, sub.get("data").get("count"),
-                   sub.get("data").get("after"))
+    response = sub_reddit.json()
+    if "data" in response:
+        data = response.get("data")
+        if not data.get("children"):
+            return (hot_list)
+        for post in data.get("children"):
+            hot_list += [post.get("data").get("title")]
+        if not data.get("after"):
+            return (hot_list)
+        subreddit[1] = data.get("after")
+        recurse(subreddit, hot_list)
+        if hot_list[-1] is None:
+            del hot_list[-1]
+        return (hot_list)
+    else:
+        return (None)
